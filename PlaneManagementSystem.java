0c13d145f1e282d13ba46513f1836838a800a6ab
za91 <160023908+za91@users.noreply.github.com>
2024-12-04 19:52:53 +0300

    Update PlaneManagementSystem.java

--- PlaneManagementSystem.java
+++ PlaneManagementSystem.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class PlaneManagementSystem {
+	private ArrayList<Plane> planeList;
+
+	
+	
+	public PlaneManagementSystem() {
+		planeList=new ArrayList<>();
+	}
+
+	public ArrayList<Plane> getPlaneList() {
+		return planeList;
+	}
+	
+	public void addPlane(Plane plane) {
+		planeList.add(plane);
+	}
+	public void removePlane(Plane plane) {
+		planeList.remove(plane);
+	}
+	
 }
+
