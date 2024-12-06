542d663caa47327ee9fad9f0ddd813ef48d1e498
HuseynRasulzade <huseyn.rasulzade@ozu.edu.tr>
2024-12-06 15:27:13 +0300

    PassengerManagementController class is added

---
+++ PassangerManagmentController.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+
+import javax.swing.table.DefaultTableModel;
+
+public class PassengerManagementController<T, E> implements ControllerInterface<AirportPassenger, PassengerManagementSystem> {
+    private PassengerManagementSystem passengerManagementSystem;
+
+    public PassengerManagementController(PassengerManagementSystem pms) {
+
+        this.passengerManagementSystem = pms;
+    }
+
+    @Override
+    public void deleteData(Integer index) {
+        // TODO Auto-generated method stub
+        ArrayList<AirportPassenger> passengers = passengerManagementSystem.getPassengerList();
+        passengerManagementSystem.removePassenger(passengers.get(index));
+
+    }
+
+
+    @Override
+    public void updateData(AirportPassenger passenger,Integer index) {
+        // TODO Auto-generated method stub
+        ArrayList<AirportPassenger> passengers = passengerManagementSystem.getPassengerList();
+        passengers.set(index, passenger);
+
+    }
+
+    @Override
+    public DefaultTableModel refresh() {
+        // TODO Auto-generated method stub
+        String[] columnNames = { "Passenger ID","Name", "Surname", "Passenger Type", "Luggage Count",
+                "Year of Birth" };
+        DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+
+        for (AirportPassenger passenger: passengerManagementSystem.getPassengerList()) {
+            String type=getPassengerType(passenger);
+
+            model.addRow(new String[] { String.valueOf(passenger.getPassengerID()),passenger.getName(), passenger.getSurname(),getPassengerType(passenger),
+                    String.valueOf(passenger.getLuggageCount()),String.valueOf(passenger.getYearOfBirth()) });
+        }
+        return model;
+    }
+
+    private String getPassengerType(AirportPassenger passenger) {
+        if (passenger.getClass().equals(BusinessPassenger.class)) {
+            return("Business");
+        }
+        if (passenger.getClass().equals(EconomyPassenger.class)) {
+            return("Economy");
+        }
+        if (passenger.getClass().equals(FamilyPassenger.class)) {
+            return("Family");
+        }
+        return "";
+    }
+
+    @Override
+    public void addData(AirportPassenger passenger) {
+        // TODO Auto-generated method stub
+        passengerManagementSystem.addPassenger(passenger);
+
+    }
+
+
+
+}
--- PassengerManagementSystem.java
+++ PassengerManagementSystem.java
@@ -1,2 +1,24 @@
-public class PassangerManagementSystem {
+import java.util.ArrayList;
+
+public class PassengerManagementSystem {
+    private ArrayList<AirportPassenger> passengerList;
+
+
+
+    public PassengerManagementSystem() {
+        passengerList=new ArrayList<>();
+    }
+
+    public ArrayList<AirportPassenger> getPassengerList() {
+        return passengerList;
+    }
+
+    public void addPassenger(AirportPassenger passenger) {
+        passengerList.add(passenger);
+    }
+
+    public void removePassenger(AirportPassenger passenger) {
+        passengerList.remove(passenger);
+    }
+
 }
