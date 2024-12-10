aec1c1f0ac1d6f0316c93611b147009ae72b59cf
raghad-elshanawany-ozu <160737499+raghad-elshanawany-ozu@users.noreply.github.com>
2024-12-10 22:34:08 +0300

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,2 +1,31 @@
-public class Ticket {
+
+public abstract class Ticket {
+	private AirportPassenger passenger;
+	private int seatNumber;
+	private double ticketPrice;
+	
+	public double getTicketPrice() {
+		return ticketPrice;
+	}
+	public void setTicketPrice(double price) {
+		this.ticketPrice = price;
+	}
+	public Ticket(int seatNumber) {
+		super();
+		this.seatNumber = seatNumber;
+	}
+	public AirportPassenger getPassenger() {
+		return passenger;
+	}
+	public void setPassenger(AirportPassenger passenger) {
+		this.passenger = passenger;
+	}
+	public int getSeatNumber() {
+		return seatNumber;
+	}
+	public void setSeatNumber(int seatNumber) {
+		this.seatNumber = seatNumber;
+	}
+	protected abstract double calculatePrice(double ticketPrice, AirportPassenger passenger2);
+	
 }
