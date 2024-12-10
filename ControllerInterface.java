c4f9c9e33ce23aed78010d85d1f34b13100435d0
raghad-elshanawany-ozu <160737499+raghad-elshanawany-ozu@users.noreply.github.com>
2024-12-10 22:31:45 +0300

    Update ControllerInterface.java

--- ControllerInterface.java
+++ ControllerInterface.java
@@ -1,2 +1,9 @@
-public interface ControllerInterface {
+import javax.swing.table.DefaultTableModel;
+
+public interface ControllerInterface<T, E> {
+	public void addData(T data);
+	public void updateData(T data, Integer index);
+	public void deleteData(Integer index);
+	public DefaultTableModel refresh();
+
 }
